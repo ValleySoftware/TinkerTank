0f5dbedd39fb00e4247e962219a132b5d8b8653b
Rob <rlmartain@hotmail.com>
2021-11-28 23:35:33 +1100

    Create MovementAbstractions.cs

---
+++ TinkerTank/Abstractions/MovementAbstractions.cs
@@ -0,0 +1,27 @@
+﻿using Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TinkerTank.Abstractions
+{
+    public class MovementAbstractions : TinkerBase, ITinkerBase
+    {
+
+
+        public void ErrorEncountered()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void RefreshStatus()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Test()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
