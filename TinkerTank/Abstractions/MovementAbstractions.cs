b9075f53f8dd77a42d943f355ea52a68f30dddfc
Rob <rlmartain@hotmail.com>
2021-12-02 13:37:47 +1100

    Update MovementAbstractions.cs

--- TinkerTank/Abstractions/MovementAbstractions.cs
+++ TinkerTank/Abstractions/MovementAbstractions.cs
@@ -1,4 +1,6 @@
 ﻿using Base;
+using Enumerations;
+using Peripherals;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,7 +9,48 @@ namespace TinkerTank.Abstractions
 {
     public class MovementAbstractions : TinkerBase, ITinkerBase
     {
+        public IMovementInterface MovementController { get; set; }
+        public decimal AutoStopDistance { get; set; }
+        private int defaultPower = 75;
+        private bool defaultSafeMove = true;
+        private int defaultAccelleration = 10;
+        private TimeSpan defaultMovementDuration = TimeSpan.FromSeconds(1);
 
+        public MovementAbstractions(IMovementInterface movementController)
+        {
+            if (movementController == null)
+            {
+                throw new NullReferenceException();
+            }
+
+            MovementController = movementController;
+        }
+
+        public void Move(Direction directionToMove)
+        {
+            Move(directionToMove, defaultPower, defaultSafeMove, defaultAccelleration, defaultMovementDuration);
+        }
+
+        public void Move(Direction directionToMove, int power)
+        {
+            Move(directionToMove, power, defaultSafeMove, defaultAccelleration, defaultMovementDuration);
+        }
+
+        public void Move(Direction directionToMove, int power, bool safeMove)
+        {
+            Move(directionToMove, power, safeMove, defaultAccelleration, defaultMovementDuration);
+        }
+
+        public void Move(Direction directionToMove, int power, bool safeMove, int accelleration)
+        {
+            Move(directionToMove, power, safeMove, accelleration, defaultMovementDuration);
+        }
+
+        public void Move(Direction directionToMove, int power, bool safeMove, int accelleration, TimeSpan movementDuration )
+        {
+            MovementController.Move(directionToMove, power, safeMove, accelleration, movementDuratio)
+            //Move(directionToMove, power, safeMove, accelleration, movementDuration);
+        }
 
         public void ErrorEncountered()
         {
